4dfeb2fc44abd5fcf05b58629c83abc2509224bc
Oleksii26 <Kolesnik26@ukr.net>
2022-08-19 21:45:43 +0300

    save

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientsElement = ingredients.map(ingredient => {
+  const ingridientsItem = document.createElement('li');
+  ingridientsItem.classList.add('.item');
+  ingridientsItem.textContent = `${ingredient}`;
+
+  return ingridientsItem;
+});
+ingredientsList.append(...ingredientsElement);
+
+
+
+
+
+
+
