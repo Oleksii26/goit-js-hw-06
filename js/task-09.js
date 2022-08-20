8a9ab425a00da3101d9cd99786b362c0393544d6
Oleksii26 <Kolesnik26@ukr.net>
2022-08-20 17:52:22 +0300

    save

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector('.change-color');
+const outputRef = document.querySelector('.color');
+
+function onChangeColor() {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  outputRef.textContent = randomColor;
+}
+
+btnRef.addEventListener('click', onChangeColor);
